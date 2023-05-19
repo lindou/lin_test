044ce158542729c30dfc1527f6806e1fc0dea904
Lin Dou <lin.dou@ecoatm.com>
2023-05-19 11:33:32 -0400

    semantic test

---
+++ release.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  branches: [
+    'master', // replace with your default branch name
+    { name: 'develop', prerelease: true, channel: 'develop' },
+  ], 
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: 'bug fix', release: 'patch' },
+          { type: 'feature', release: 'minor' },
+          { type: 'break', release: 'major' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
